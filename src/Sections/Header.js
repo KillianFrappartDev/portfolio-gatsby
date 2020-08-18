2e0933e32b55c231fa1f8731f2fc3cc151a4bea7
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-08-18 15:32:02 +0100

    Improved

--- src/Sections/Header.js
+++ src/Sections/Header.js
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
-import headerStyle from '../Styles/header.module.css';
+import headerStyle from "../Styles/header.module.css";
 
 const Header = () => {
-    return (
-        <header className={headerStyle.section}>
-            <div className={headerStyle.container}>
-                <div className={headerStyle.invisibleTop}></div>
-                <h1 className={headerStyle.title}>Full-Stack Developer</h1>
-                <div className={headerStyle.invisibleBottom}></div>
-            </div>
-        </header>
-    )
-}
+  return (
+    <header className={headerStyle.section}>
+      <div className={headerStyle.filter}>
+        <div className={headerStyle.container}>
+          <h1 className={headerStyle.title}>Full-Stack Developer</h1>
+        </div>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
