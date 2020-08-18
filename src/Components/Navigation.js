e512f672f5a45dcded796df93b8f8674c67c09bf
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-08-18 17:59:59 +0100

    about

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,38 +1,71 @@
 import React, { useState } from "react";
+import { Helmet } from "react-helmet";
 
 import navigationStyle from "../Styles/navigation.module.css";
 
 const Navigation = () => {
-    const [isScrolled, setIsScrolled] = useState(false);
+  const [isScrolled, setIsScrolled] = useState(false);
 
-    const checkScroll = () => {
-        console.log(window.scrollY);
-        window.scrollY > 100 ? setIsScrolled(true) : setIsScrolled(false);
-    }
+  const checkScroll = () => {
+    console.log(window.scrollY);
+    window.scrollY > 100 ? setIsScrolled(true) : setIsScrolled(false);
+  };
 
-    window.addEventListener("scroll", checkScroll);
+  window.addEventListener("scroll", checkScroll);
 
   return (
-    <div className={navigationStyle.container}>
-      <nav className={isScrolled ? navigationStyle.navblack : navigationStyle.navbar}>
-        <a href="#about" className={isScrolled ? navigationStyle.linkblack : navigationStyle.link}>
-        <i class="fas fa-user fa-2x"></i>
-          <h3  className={navigationStyle.section}>About</h3>
-        </a>
-        <div className={isScrolled ? navigationStyle.linkblack : navigationStyle.link}>
-        <i class="fas fa-code fa-2x"></i>
-          <h3 className={navigationStyle.section}>Skills</h3>
-        </div>
-        <div className={isScrolled ? navigationStyle.linkblack : navigationStyle.link}>
-        <i class="fas fa-hammer fa-2x"></i>
-          <h3 className={navigationStyle.section}>Projects</h3>
-        </div>
-        <div className={isScrolled ? navigationStyle.linkblack : navigationStyle.link}>
-        <i class="fas fa-paper-plane fa-2x"></i>
-          <h3 className={navigationStyle.section}>Contact</h3>
-        </div>
-      </nav>
-    </div>
+    <React.Fragment>
+      <Helmet>
+        <link
+          rel="stylesheet"
+          href="https://use.fontawesome.com/releases/v5.14.0/css/all.css"
+          integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc"
+          crossorigin="anonymous"
+        />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&family=Roboto:wght@400;700&display=swap" rel="stylesheet"></link>
+      </Helmet>
+      <div className={navigationStyle.container}>
+        <nav
+          className={
+            isScrolled ? navigationStyle.navblack : navigationStyle.navbar
+          }
+        >
+          <a
+            href="#about"
+            className={
+              isScrolled ? navigationStyle.linkblack : navigationStyle.link
+            }
+          >
+            <i className="fas fa-user fa-2x"></i>
+            <h3 className={navigationStyle.section}>About</h3>
+          </a>
+          <div
+            className={
+              isScrolled ? navigationStyle.linkblack : navigationStyle.link
+            }
+          >
+            <i className="fas fa-code fa-2x"></i>
+            <h3 className={navigationStyle.section}>Skills</h3>
+          </div>
+          <div
+            className={
+              isScrolled ? navigationStyle.linkblack : navigationStyle.link
+            }
+          >
+            <i className="fas fa-hammer fa-2x"></i>
+            <h3 className={navigationStyle.section}>Projects</h3>
+          </div>
+          <div
+            className={
+              isScrolled ? navigationStyle.linkblack : navigationStyle.link
+            }
+          >
+            <i className="fas fa-paper-plane fa-2x"></i>
+            <h3 className={navigationStyle.section}>Contact</h3>
+          </div>
+        </nav>
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/Sections/About.js
+++ src/Sections/About.js
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 import aboutStyle from "../Styles/about.module.css";
 
 const About = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const checkScroll = () => {
+        window.scrollY > 500 && setIsVisible(true);
+    }
+
+    window.addEventListener("scroll", checkScroll);
+
   return (
     <section id="about" className={aboutStyle.about}>
-      <div className={aboutStyle.content}>
+      <div className={isVisible ? aboutStyle.content : aboutStyle.hide}>
         <p>
-          My name is Killian, I am 24 years old and I am a self taught
+  <span className={aboutStyle.open}>{"{"}</span>My name is Killian, I am 24 years old and I am a self taught
           Full-Stack Web developer.
         </p>
         <p>
@@ -15,14 +23,13 @@ const About = () => {
           fell in love with coding.
         </p>
         <p>
-          Born in Belgium, my native language is french but I learned english
-          when I was working in Australia a couple years ago.
+          Born in Belgium, my native language is french but I am fluent in english
+          and I am currently learning dutch.
         </p>
         <p>
-          I don't know evrything but there is nothing I can't learn. My interest
-          for development has no limit.
+          I don't know everything but there is nothing I can't learn.
         </p>
-        <p>Click here and have a look to my resume.</p>
+        <p>Click <span className={aboutStyle.link}>here</span> and have a look to my resume.<span className={aboutStyle.close}>{"}"}</span></p>
       </div>
     </section>
   );
