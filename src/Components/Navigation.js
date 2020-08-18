f49bc2b4f3123e42499c06a34c747996b0ed7abc
KillianFrappartDev <KillianFrappartDev@gmail.com>
2020-08-18 13:58:04 +0100

    header

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -4,9 +4,26 @@ import navigationStyle from "../Styles/navigation.module.css";
 
 const Navigation = () => {
   return (
-      <div className={navigationStyle.container}>
-          <nav className={navigationStyle.navbar}></nav>
-      </div>
+    <div className={navigationStyle.container}>
+      <nav className={navigationStyle.navbar}>
+        <div className={navigationStyle.link}>
+        <i class="fas fa-user fa-2x"></i>
+          <h3 className={navigationStyle.section}>About</h3>
+        </div>
+        <div className={navigationStyle.link}>
+        <i class="fas fa-code fa-2x"></i>
+          <h3 className={navigationStyle.section}>Skills</h3>
+        </div>
+        <div className={navigationStyle.link}>
+        <i class="fas fa-hammer fa-2x"></i>
+          <h3 className={navigationStyle.section}>Projects</h3>
+        </div>
+        <div className={navigationStyle.link}>
+        <i class="fas fa-paper-plane fa-2x"></i>
+          <h3 className={navigationStyle.section}>Contact</h3>
+        </div>
+      </nav>
+    </div>
   );
 };
 
